707076cadd2e1d0788599d49e5591ab56bc2d939
Fabio Vallejo <fabiovallejo19873@gmail.com>
2024-05-11 11:09:17 -0500

    Update agregandoarchivo.py

--- agregandoarchivo.py
+++ agregandoarchivo.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-#agregando archivo de python para descargar
-nombre="andres"
-print ("mi nombre es", nombre)
-=======
+
 agregando archivo de python para descargar
 NOmbre= "fabio"
 print ("nombre")
->>>>>>> fd9e49392f1fa684714579376fb5d6a0b348322e
+
